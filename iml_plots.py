3f9f82c4e4fbdb7cee20b93e5f203b5d8a445364
JakubKucharski9 <s27445@pjwstk.edu.pl>
2024-12-27 16:15:17 +0100

    final

--- iml_plots.py
+++ iml_plots.py
@@ -8,7 +8,7 @@ import seaborn as sns
 
 # Connect to the database and load data
 with sqlite3.connect('database.sqlite') as conn:
-    query = "SELECT * FROM Match_with_overall"
+    query = "SELECT * FROM Match_for_plots"
     data = pd.read_sql_query(query, conn)
 
 # Remove rows with missing values
@@ -30,11 +30,9 @@ for home_col, away_col in zip(home_features, away_features):
     diff_col_name = f'diff_{feature_name}'
     data[diff_col_name] = data[home_col] - data[away_col]
 
-data['field_importance'] = abs(data['home_avg_overall_rating'] - data['away_avg_overall_rating'])
 
 # Use only difference features and other relevant features
 diff_features = [col for col in data.columns if col.startswith('diff_')]
-feature_columns = diff_features + ['field_importance']
 
 # Ensure equal representation of classes in the dataset
 class_counts = data['prediction'].value_counts()
@@ -50,14 +48,14 @@ balanced_data = pd.concat([
 balanced_data = balanced_data.sample(frac=1, random_state=42).reset_index(drop=True)
 
 # Update X and y with balanced data
-X = balanced_data[feature_columns]
+X = balanced_data[diff_features]
 y = balanced_data['prediction']
 
 # Split the dataset into training and testing sets with stratification
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
 # Define and train Random Forest classifier
-rf_classifier = RandomForestClassifier(n_estimators=50, max_depth=6, random_state=42, class_weight='balanced')
+rf_classifier = RandomForestClassifier(n_estimators=200, max_depth=15, random_state=42, class_weight='balanced')
 rf_classifier.fit(X_train, y_train)
 
 # Predict on the test set
@@ -86,7 +84,7 @@ feature_importances = rf_classifier.feature_importances_
 
 # Create a DataFrame for better visualization
 importances_df = pd.DataFrame({
-    'Feature': feature_columns,
+    'Feature': diff_features,
     'Importance': feature_importances
 }).sort_values(by='Importance', ascending=False)
 
